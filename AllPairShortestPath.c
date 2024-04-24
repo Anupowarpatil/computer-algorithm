1b265ca051934f41a1ab71c5d93f92df20f9b78f
Purva kiran powar patil <109520470+Anupowarpatil@users.noreply.github.com>
2024-04-24 12:51:48 +0530

    Add files via upload

---
+++ AllPairShortestPath.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define INF 99999
+#define V 4
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+    int i, j, k;
+
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printf("Shortest distances between every pair of vertices:\n");
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d   ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
+
